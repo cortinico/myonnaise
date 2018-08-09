55f6add8474fb136983fca534ad7ce2be14e61b8
Nicola Corti <corti.nico@gmail.com>
2018-08-10 00:34:43 +0200

    Updating several dependencies

--- buildSrc/src/main/java/Dependecies.kt
+++ buildSrc/src/main/java/Dependecies.kt
@@ -1,18 +1,26 @@
 object Versions {
-    const val androidx = "28.0.0-alpha3"
+    const val androidGradle = "3.2.0-beta02"
+    const val androidMavenGradle = "2.0"
+    const val androidx = "28.0.0-rc01"
     const val appintro = "v4.2.3"
+    const val bintrayGradle = "1.8.4"
     const val dagger = "2.16"
     const val constraintlayout = "1.1.2"
     const val junit = "4.12"
-    const val kotlin = "1.2.51"
+    const val kotlin = "1.2.60"
     const val materialcomponents = "1.0.0-alpha3"
-    const val mockito = "2.19.0"
+    const val mockito = "2.21.0"
     const val mockitoKotlin = "2.0.0-RC1"
-    const val rxjava2 = "2.1.15"
+    const val rxjava2 = "2.2.0"
     const val rxandroid2 = "2.0.2"
 }
 
 object Libs {
+    const val androidGradlePlugin = "com.android.tools.build:gradle:${Versions.androidGradle}"
+    const val androidMavenGradlePlugin = "com.github.dcendents:android-maven-gradle-plugin:${Versions.androidMavenGradle}"
+    const val bintrayGradlePlugin = "com.jfrog.bintray.gradle:gradle-bintray-plugin:${Versions.bintrayGradle}"
+    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+
     const val kotlinJdk = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
 
     const val androidXAppCompat = "com.android.support:appcompat-v7:${Versions.androidx}"
