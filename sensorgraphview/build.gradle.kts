f16e82d40382ff08d2b54307cbe8a981caecb922
Nicola Corti <corti.nico@gmail.com>
2019-01-16 20:29:59 +0100

    Converting Gradle to Kotlin DSL - Part 1

---
+++ sensorgraphview/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("com.github.ben-manes.versions")
+}
+
+android {
+    compileSdkVersion(28)
+
+    defaultConfig {
+        minSdkVersion(21)
+        targetSdkVersion(28)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        this.register("release ") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(Libs.kotlinJdk)
+    implementation(Libs.androidXAppCompat)
+
+    testImplementation(Libs.junit)
+}
