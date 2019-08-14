6dd8f49204ac81d535b8637a1b0cab317b327488
Nicola Corti <corti.nico@gmail.com>
2019-08-14 17:07:46 +0200

    RxJava2 to 2.2.11

--- buildSrc/src/main/java/Dependecies.kt
+++ buildSrc/src/main/java/Dependecies.kt
@@ -11,7 +11,7 @@ object Versions {
     const val materialcomponents = "1.0.0"
     const val mockito = "2.23.4"
     const val mockitoKotlin = "2.1.0"
-    const val rxjava2 = "2.2.9"
+    const val rxjava2 = "2.2.11"
     const val rxandroid2 = "2.1.1"
     const val versionsUpdate = "0.20.0"
 }
